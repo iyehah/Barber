4f9c19f483c9cf5e54c252470fae49c3a8c40940
Iyehah <117352746+iyehah@users.noreply.github.com>
2025-05-14 17:44:36 +0000

    refactor:project styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "@/styles/globals.css"
 import { LanguageProvider } from "@/contexts/language-context"
 
 const inter = Inter({ subsets: ["latin"] })
